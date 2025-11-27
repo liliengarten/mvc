76e0668453e803ac98a716ec45f8b98eeb0fea2f
gr643_cia <liliengarten333@gmail.com>
2025-11-27 13:45:26 +0700

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'liliengarten/mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ff07d4af80d8bfa90eb859dbd4c5de2089f9bf6b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'liliengarten/mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ff07d4af80d8bfa90eb859dbd4c5de2089f9bf6b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
